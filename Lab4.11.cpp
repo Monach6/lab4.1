647f2c82c547e5d0088ec0feeb62bd70a5cec253
Monach6 <prtroman@gmail.com>
2022-10-22 15:26:34 +0300

    lab4.1

---
+++ Lab4.11.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	int i, k;
+	double S;
+
+	cout << "k = "; cin >> k; cout << endl;
+
+	S = 0;
+	i = k;
+	while (i <= 15)
+	{
+		S += cos(i) / (1 + sin(i) * sin(i));
+		i++;
+	}
+	cout << "S = " << S << endl;
+
+	S = 0;
+	i = k;
+	do
+	{
+		S += cos(i) / (1 + sin(i) * sin(i));
+		i++;
+	} while (i <= 15);
+	cout << "S = " << S << endl;
+
+	S = 0;
+	for (i = k; i <= 15; i++)
+	{
+		S += cos(i) / (1 + sin(i) * sin(i));
+	}
+	cout << "S = " << S << endl;
+
+	S = 0;
+	for (i = 15; i >= k; i--)
+	{
+		S += cos(i) / (1 + sin(i) * sin(i));
+	}
+	cout << "S = " << S << endl;
+
+	cin.get();
+	system("pause");
+	return 0;
+}
